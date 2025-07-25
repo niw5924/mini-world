5689dfad46610e53a8fad9f034dc056cad03f46e
niw5924 <niw5924@naver.com>
2025-07-26 02:00:10 +0900

    feat: 카드플립 화면 뒤집기 애니메이션 추가

--- lib/game/card_flip/card_flip_game_screen.dart
+++ lib/game/card_flip/card_flip_game_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:mini_world/constants/app_colors.dart';
 import 'package:mini_world/widgets/mini_world_button.dart';
@@ -9,11 +10,62 @@ class CardFlipGameScreen extends StatefulWidget {
   State<CardFlipGameScreen> createState() => _CardFlipGameScreenState();
 }
 
-class _CardFlipGameScreenState extends State<CardFlipGameScreen> {
-  final List<_CardItem> cards = List.generate(
-    10,
-    (index) => _CardItem(id: index + 1),
-  );
+class _CardFlipGameScreenState extends State<CardFlipGameScreen>
+    with TickerProviderStateMixin {
+  late List<_CardItem> cards;
+  late List<AnimationController> controllers;
+  late List<Animation<double>> animations;
+
+  @override
+  void initState() {
+    super.initState();
+    final ids = List.generate(10, (index) => index + 1)..shuffle();
+    cards = ids.map((id) => _CardItem(id: id, isFlipped: false)).toList();
+
+    controllers = List.generate(cards.length, (_) {
+      return AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 300),
+      );
+    });
+
+    animations =
+        controllers
+            .map(
+              (controller) =>
+                  Tween<double>(begin: 0.0, end: pi).animate(controller),
+            )
+            .toList();
+  }
+
+  @override
+  void dispose() {
+    for (final controller in controllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  void _flipCard(int index) {
+    final controller = controllers[index];
+    final card = cards[index];
+
+    if (controller.isAnimating) return;
+
+    if (!card.isFlipped) {
+      controller.forward().then((_) {
+        setState(() {
+          card.isFlipped = true;
+        });
+      });
+    } else {
+      controller.reverse().then((_) {
+        setState(() {
+          card.isFlipped = false;
+        });
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +80,10 @@ class _CardFlipGameScreenState extends State<CardFlipGameScreen> {
           padding: const EdgeInsets.all(24),
           child: Column(
             children: [
+              const Text(
+                '카드 중 하나를 선택해보세요!',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
               Expanded(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -41,40 +97,39 @@ class _CardFlipGameScreenState extends State<CardFlipGameScreen> {
                             crossAxisCount: 5,
                             crossAxisSpacing: 12,
                             mainAxisSpacing: 12,
+                            childAspectRatio: 0.65,
                           ),
                       itemBuilder: (context, index) {
                         final card = cards[index];
+                        final animation = animations[index];
+
                         return GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              card.isFlipped = !card.isFlipped;
-                            });
-                          },
-                          child: Card(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16),
-                              side: BorderSide(
-                                color:
-                                    card.isFlipped
-                                        ? AppColors.primary
-                                        : Colors.transparent,
-                                width: 3.0,
-                              ),
-                            ),
-                            clipBehavior: Clip.antiAlias,
-                            color:
-                                card.isFlipped
-                                    ? Colors.amber.shade200
-                                    : Colors.grey.shade300,
-                            child: Center(
-                              child: Text(
-                                card.isFlipped ? card.id.toString() : '?',
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
+                          onTap: () => _flipCard(index),
+                          child: AnimatedBuilder(
+                            animation: animation,
+                            builder: (context, _) {
+                              final angle = animation.value;
+                              final isFront = angle <= pi / 2;
+
+                              return Transform(
+                                alignment: Alignment.center,
+                                transform: Matrix4.rotationY(angle),
+                                child:
+                                    isFront
+                                        ? _buildCardFace(
+                                          text: '?',
+                                          isFront: true,
+                                        )
+                                        : Transform(
+                                          alignment: Alignment.center,
+                                          transform: Matrix4.rotationY(pi),
+                                          child: _buildCardFace(
+                                            text: card.id.toString(),
+                                            isFront: false,
+                                          ),
+                                        ),
+                              );
+                            },
                           ),
                         );
                       },
@@ -89,11 +144,31 @@ class _CardFlipGameScreenState extends State<CardFlipGameScreen> {
       ),
     );
   }
+
+  Widget _buildCardFace({required String text, required bool isFront}) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+        side:
+            isFront
+                ? BorderSide.none
+                : BorderSide(color: AppColors.primary, width: 3.0),
+      ),
+      clipBehavior: Clip.antiAlias,
+      color: isFront ? Colors.grey.shade300 : AppColors.secondary,
+      child: Center(
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
 }
 
 class _CardItem {
   final int id;
   bool isFlipped;
 
-  _CardItem({required this.id, this.isFlipped = false});
+  _CardItem({required this.id, required this.isFlipped});
 }
